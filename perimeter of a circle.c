77ba4c675d16e76da3b04f278d863c4184ac548a
greyhatreyyan <144786906+greyhatreyyan@users.noreply.github.com>
2023-09-13 11:10:09 +0300

    Add files via upload

---
+++ perimeter of a circle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int d;
+    float perimeter;
+    const float PI = 3.14;
+    printf("the value of d");
+    scanf("%d",&d);
+    perimeter = PI*d;
+    printf("perimeter = %.2f \n",perimeter);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
